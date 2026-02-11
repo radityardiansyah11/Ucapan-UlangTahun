5897a13753c35dabf7009e9587ad6c5ca7bb8ef0
Raditya Ardiansyah <raditya.ardiansyah1234@gmail.com>
2026-02-11 22:43:25 +0700

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -14,7 +14,7 @@ function validateForm() {
     Swal.fire({
       icon: "success",
       title: "Login berhasil!",
-      text: "Selamat datang ayanggg",
+      text: "Welcome, Yaya",
       showConfirmButton: false,
       timer: 1500,
     }).then(function () {
